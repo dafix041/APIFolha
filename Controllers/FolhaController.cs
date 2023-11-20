5023997007dfc02f5578abbc4b89701af455cf50
dafix.041 <vcruzz1977@gmail.com>
2023-11-20 02:46:47 -0300

    boa noite pt2

--- Controllers/FolhaController.cs
+++ Controllers/FolhaController.cs
@@ -43,5 +43,48 @@ public class FolhaController : ControllerBase
         }
         return Ok(folhas);
     }
+[HttpPut]
+[Route("alterar/{id}")]
+public IActionResult AlterarFolha(int id, [FromBody] Folha folha)
+{
+    try
+    {
+        if (folha == null)
+        {
+            return BadRequest("O objeto Folha não pode ser nulo.");
+        }
+
+        // Verifica se existe uma folha de pagamento com o mesmo ID antes de fazer a alteração
+        var existingFolha = _ctx.Folhas.FirstOrDefault(f => f.FolhaId == id);
+
+        if (existingFolha == null)
+        {
+            return NotFound("Folha de pagamento não encontrada para alteração.");
+        }
+
+        // Realiza a alteração nos campos necessários
+        existingFolha.Valor = folha.Valor;
+        existingFolha.Quantidade = folha.Quantidade;
+        existingFolha.Mes = folha.Mes;
+        existingFolha.Ano = folha.Ano;
+        existingFolha.FuncionarioId = folha.FuncionarioId;
+        existingFolha.SalarioBruto = folha.SalarioBruto;
+
+        _ctx.Folhas.Update(existingFolha);
+        _ctx.SaveChanges();
+
+        return Created("", existingFolha);
+    }
+    catch (Exception e)
+    {
+        return BadRequest($"Ocorreu um erro ao tentar alterar a folha de pagamento: {e.Message}");
+    }
+}
+
+
+
+
+
+
 
 }
